ad314e1730ecef16639fa13d9f32dda0fa155f22
MixonUa <mykhailofrolov@gmail.com>
2023-09-15 12:32:05 +0300

    add project structure

---
+++ NewslineTestApp/Controllers/NewsFeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewsFeedViewController.swift
+//  NewslineTestApp
+//
+//  Created by Михаил Фролов on 15.09.2023.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
