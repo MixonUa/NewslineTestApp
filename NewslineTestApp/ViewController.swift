ddc15ea08536e5833773bba04c5b2a1e977a8595
MixonUa <mykhailofrolov@gmail.com>
2023-09-15 12:09:27 +0300

    add project body

---
+++ NewslineTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewslineTestApp
+//
+//  Created by Михаил Фролов on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
