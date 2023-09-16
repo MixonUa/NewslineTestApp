f37c7fb284637e080e76da94cbb2e7a20247eb23
MixonUa <mykhailofrolov@gmail.com>
2023-09-16 13:08:40 +0300

    add button state logic

--- NewslineTestApp/Views/Cells/NewsFeedTableViewCell.swift
+++ NewslineTestApp/Views/Cells/NewsFeedTableViewCell.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+enum ButtonState {
+    case expand
+    case collapse
+}
+
 class NewsFeedTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var informationLabel: UILabel!
@@ -14,22 +19,58 @@ class NewsFeedTableViewCell: UITableViewCell {
     @IBOutlet weak var daysAgoLabel: UILabel!
     @IBOutlet weak var textModifyButton: UIButton!
     
+    private var currentState = ButtonState.expand
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setExpandButtonState()
+        collapseText()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
+    
+    // MARK: Cell
     func updateCell(title: String, information: String, likes: Int, days: Int) {
         titleLabel.text = title
         informationLabel.text = information
         likesLabel.text = "\u{2764}" + String(likes)
         daysAgoLabel.text = String(days) + " day" + " ago"
     }
+    
+    // MARK: BUTTON
+    private func setExpandButtonState() {
+        textModifyButton.setTitle("Expand", for: .normal)
+    }
+    
+    private func setCollapseButtonState() {
+        textModifyButton.setTitle("Collapse", for: .normal)
+    }
+    
+    private func expandText() {
+        informationLabel.numberOfLines = 0
+    }
+    private func collapseText() {
+        informationLabel.numberOfLines = 2
+    }
+    
+    private func checkState() {
+        switch currentState {
+        case .expand:
+            expandText()
+            setCollapseButtonState()
+            currentState = ButtonState.collapse
+            break
+        case .collapse:
+            collapseText()
+            setExpandButtonState()
+            currentState = ButtonState.expand
+            break
+        }
+    }
+    
     @IBAction func textModifyButtonPressed(_ sender: Any) {
+        checkState()
     }
 }
